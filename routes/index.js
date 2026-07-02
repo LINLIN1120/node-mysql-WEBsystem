58c198e825b4f88608a88e66b7c05bb82a630a30
j2304ITAYA <j2304@matsue-ct.ac.jp>
2026-07-02 06:56:19 +0000

    サインイン機能を作成しよう

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ const connection = mysql.createConnection({
 });
 
 router.get('/', function (req, res, next) {
+const userId = req.session.userid;
+const isAuth = Boolean(userId);
+console.log(`isAuth: ${isAuth}`);
+
   knex("tasks")
     .select("*")
     .then(function (results) {
@@ -46,5 +50,6 @@ router.post('/', function (req, res, next) {
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 
 module.exports = router;
